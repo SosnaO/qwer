b20e702a9815991def64207d6e19942ef181b8cf
Sosna Olga <solivijasosna@gmail.com>
2022-02-12 20:51:43 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import List from "./components/List";
-import styles from "../src/components/styles.module.css";
+import styles from "./components/styles.module.css";
 function App() {
   return (
     <div className={styles.app}>
